23065ad8dcce944421953468bda82a8cb30b4a9a
codematsing <sam.solis@codematsing.com>
2023-04-11 22:21:01 +0800

    added m2m_changed

--- {{cookiecutter.project_slug}}/utils/cookiecutter-app/{%raw%}{{cookiecutter.app_name}}{%endraw%}/signals.py
+++ {{cookiecutter.project_slug}}/utils/cookiecutter-app/{%raw%}{{cookiecutter.app_name}}{%endraw%}/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, m2m_changed
 from django.dispatch import receiver
 from {{cookiecutter.app_name}}.models import {{cookiecutter.snake_case_model_name}}
 
@@ -7,4 +7,11 @@ from {{cookiecutter.app_name}}.models import {{cookiecutter.snake_case_model_nam
 #     if created:
 #         pass
 #     else:
+#         pass
+
+# @receiver(m2m_changed, sender={{cookiecutter.snake_case_model_name}})
+# def {{cookiecutter.model_name[:-1]}}_<model_fk>_changed(sender, instance, action, reverse, model, pk_set, using):
+#     if action=='post_add':
+#         pass
+#     elif action=='post_remove':
 #         pass

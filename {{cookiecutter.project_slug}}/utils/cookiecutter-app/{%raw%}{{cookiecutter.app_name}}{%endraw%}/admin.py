05b559e3a83506dac88e567875da9b66ab3f0e7f
codematsing <sam.solis@codematsing.com>
2023-04-12 15:51:37 +0800

    changed cookiecutter-app to submodule

--- {{cookiecutter.project_slug}}/utils/cookiecutter-app/{%raw%}{{cookiecutter.app_name}}{%endraw%}/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from models import {{cookiecutter.snake_case_model_name}}
-
-# Register your models here.
-@admin.site.register({{cookiecutter.snake_case_model_name}})
--- {{cookiecutter.project_slug}}/utils/cookiecutter-app/{%raw%}{{cookiecutter.app_name}}{%endraw%}/ajax/apps.py
+++
@@ -1,9 +0,0 @@
-{% raw %}
-from django.apps import AppConfig
-
-
-class {{cookiecutter.snake_case_app_name}}AjaxConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = '{{cookiecutter.app_name}}_ajax'
-
-{% endraw %}

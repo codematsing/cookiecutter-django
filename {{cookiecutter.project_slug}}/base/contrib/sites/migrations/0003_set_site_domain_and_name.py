3182bf8cbc2edeefdb4c35c65afda7b7e9940c4c
codematsing <sam.solis@codematsing.com>
2024-03-02 13:53:12 +0800

    added printing in sites migration

--- {{cookiecutter.project_slug}}/base/contrib/sites/migrations/0003_set_site_domain_and_name.py
+++ {{cookiecutter.project_slug}}/base/contrib/sites/migrations/0003_set_site_domain_and_name.py
@@ -16,6 +16,7 @@ def _update_or_create_site_with_sequence(site_model, connection, domain, name):
             "name": name,
         },
     )
+    print(f"{site.domain} with SITE_ID={site.pk}")
     if created:
         # We provided the ID explicitly when creating the Site entry, therefore the DB
         # sequence to auto-generate them wasn't used and is now out of sync. If we

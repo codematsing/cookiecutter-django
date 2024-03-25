2ef3f5163e6f918c45d72ff1a778075ce1f31885
codematsing <sam.solis@codematsing.com>
2024-03-26 00:45:34 +0800

    added crsf trusted origins

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -19,6 +19,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["{{ cookiecutter.domain_name }}"])
+CSRF_TRUSTED_ORIGINS = ["https://{{ cookiecutter.domain_name }}"]
 
 # DATABASES
 # ------------------------------------------------------------------------------

9a2357b30b7bddf061cf8b2fe7efbc59c0c195cb
codematsing <sam.solis@codematsing.com>
2024-05-17 00:18:07 +0800

    improved load_dummy

--- {{cookiecutter.project_slug}}/base/mock_data/management/commands/load_dummy.py
+++ {{cookiecutter.project_slug}}/base/mock_data/management/commands/load_dummy.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.contrib.contenttypes.models import ContentType
 from django.core.management.base import BaseCommand
+from django.core import management
 from django.conf import settings
 from faker import Faker
 from random import choices, randint, choice
@@ -15,6 +16,10 @@ Faker.seed(0)
 class Command(BaseCommand):
     help = "create dummy data"
 
+    fixture_files = [
+        "sidebar.json",
+    ]
+
     def setup_initial_users(self):
         admin_user = get_user_model().objects.get_or_create(
             username="admin",
@@ -27,6 +32,12 @@ class Command(BaseCommand):
             user.set_password("qwer!@#$")
             user.save()
 
+    def load_fixtures(self):
+        for fixture in self.fixture_files:
+            fixture_path = os.path.join(settings.BASE_DIR, "fixtures", fixture)
+            management.call_command("loaddata", fixture_path)
+
 
     def handle(self, *args, **kwargs):
         self.setup_initial_users()
+        self.load_fixtures()
